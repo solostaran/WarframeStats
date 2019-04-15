18973752ee960d6a2baef09d70d1766dfa371e1b
Joan Reynaud <solostaran14@gmail.com>
2019-04-15 14:30:42 +0200

    Nodemon integration

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ const mongoose = require('mongoose');
 //Configure isProduction variable
 const isProduction = process.env.NODE_ENV === 'production';
 if (isProduction) console.log('Production configuration.');
-else console.log('Development configuration.')
+else console.log('Development configuration.');
+
+const isNodemon = process.env.NODEMON === 'true';
 
 const app = express();
 
@@ -94,13 +96,17 @@ app.use(function(req, res) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = isProduction ? {} : err;
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
-
+if (isNodemon) {
+    const PORT = 3000;
+    app.listen(PORT, () => console.log('Server running on http://localhost:'+PORT+'/'));
+} else {
+    module.exports = app;
+}
 console.log('WarframeStats RESTful API server started.');
