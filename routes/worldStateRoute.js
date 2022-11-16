74dff00e831eca6a3e62b3efee0afb34750063ec
JRD <joan.reynaud@ensicaen.fr>
2022-11-16 17:11:39 +0100

    World State redirect to Warframestat.us' hub + update to passport 0.6 Update the world state view (even if not using it).

--- routes/worldStateRoute.js
+++ routes/worldStateRoute.js
@@ -4,15 +4,15 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-router.get('/events', async function(req, res) {
+router.get('/', async function(req, res) {
     //StatsProcess.global(ret => res.render('statsGeneral', { title: 'General Stats', stats: ret.listStats, totalCount: ret.totalCount}));
     try {
         // const response = await axios.get('http://content.warframe.com/dynamic/worldState.php');
         // console.log(response.data.Events.length + ' events.');
         // const events = response.data.Events.filter( (n,i) => { return n.Messages[0].LanguageCode === 'en'; });
 
-        const response = await axios.get('https://api.warframestat.us/pc/events/');
-        res.render('worldEvents', {title: 'World Events', events: response.data})
+        const response = await axios.get('https://api.warframestat.us/pc/');
+        res.render('worldEvents', {title: 'World Events', pc: response.data})
     } catch (error) {
         res.status(500).send(error);
     }
