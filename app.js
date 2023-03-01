20c5d5b4b3dd6a2f956cfca6e6835d7fcf1d083f
JRD <joan.reynaud@ensicaen.fr>
2023-03-01 17:22:50 +0100

    Update : HSTS, CSP, X-Frame, XSS protection. Still a problem with inline scripts.

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ else console.log('Development configuration.');
 
 const isNodemon = process.env.NODEMON === 'true';
 const isDocker = process.env.DOCKER === 'true';
+const isHttp = process.env.HTTP === 'true';
 
 const app = express();
 
@@ -32,12 +33,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-if (isProduction)
+if (isProduction) {
 	app.use(morgan('combined', {
-		skip: function (req, res) { return res.statusCode < 400 }
-	}));
-else
-	app.use(morgan('dev'));
+		skip: function (req, res) {
+			return res.statusCode < 400
+		}
+	}))
+	if (!isHttp) {
+		// HTTPS -> HSTS header
+		app.use(function(req, res, next) {
+			if (req.secure) {
+				res.setHeader('Strict-Transport-Security', 'max-age=63072000; includeSubDomains') // 2 years
+			}
+			next()
+		})
+	}
+} else {
+	app.use(morgan('dev'))
+}
+
+const crypto = require("crypto");
+app.use(function(req, res, next) {
+	// Maybe use the Helmet middleware instead ?
+	// from https://www.npmjs.com/package/helmet
+	// and https://www.npmjs.com/package/helmet-csp
+	res.locals.nonce = crypto.randomBytes(16).toString("hex");
+	res.setHeader('X-Content-Type-Options', 'nosniff');
+	//res.setHeader('Content-Security-Policy', `default-src 'self' ; img-src 'self' data: ; style-src 'self' https://cdn.jsdelivr.net https://unpkg.com https://code.jquery.com 'unsafe-inline' ; script-src 'self' https://cdn.jsdelivr.net https://unpkg.com https://code.jquery.com 'nonce-${res.locals.nonce}' ; font-src https://cdn.jsdelivr.net ; object-src 'self' ; frame-ancestors 'self'`);
+	res.setHeader('Content-Security-Policy', `default-src 'self' ; img-src 'self' data: ; style-src 'self' https://cdn.jsdelivr.net https://unpkg.com https://code.jquery.com 'unsafe-inline' ; script-src 'self' https://cdn.jsdelivr.net https://unpkg.com https://code.jquery.com 'unsafe-inline' ; font-src https://cdn.jsdelivr.net ; object-src 'self' ; frame-ancestors 'self'`);
+	// CSP directive "img-src data:" may be vulnerable to XSS !
+	// CSP directive "style-src 'unsafe-inline'" must be explored, but it depends on the 'bootstrap-table' module.
+	// CSP directive "script-src ''unsafe-inline'" must be excluded, workaround is to set the onClick via a script. @see https://stackoverflow.com/questions/47021481/content-security-policy-with-dynamic-button
+	res.setHeader('X-Frame-Options','SAMEORIGIN');
+	res.setHeader('X-XSS-Protection','1; mode=block');
+	next()
+})
 
 // Configure MONGOOSE (on docker, use a different hostname)
 mongoose.Promise = global.Promise;
